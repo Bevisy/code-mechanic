dee7b6399d8cf474cf5e87f5ee4d03bb4bf33dbe
bevisy <binbin36520@gmail.com>
2020-08-17 11:11:38 +0800

    add t0001
    Signed-off-by: bevisy <binbin36520@gmail.com>

---
+++ leetcode/t0001.go
@@ -0,0 +1,14 @@
+package leetcode
+
+// 顺序扫描数组，对每一个元素，在 map 中找能组合给定值的另一半数字，如果找到了，直接返回 2 个数字的下标即可。如果找不到，就把这个数字存入 map 中，等待扫到“另一半”数字的时候，再取出来返回结果
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
